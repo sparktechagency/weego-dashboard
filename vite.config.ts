5615c5b03668b11753372fc2690b350c43bfc63f
rajin572 <din.islam.rajin572@gmail.com>
2026-01-21 12:04:44 +0600

    feat: Update Vite configuration to enhance server and preview settings with allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // allows access via IP or hostname
+    port: 3000,
+    strictPort: true,
+    allowedHosts: ["admin.weego.com.mx"], // ✅ add your domain here
+  },
+  preview: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+    allowedHosts: ["admin.weego.com.mx"], // ✅ also for preview
+  },
 });
